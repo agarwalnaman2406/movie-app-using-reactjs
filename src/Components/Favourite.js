0999a337c7f216e92cbf57ce50605e67fb1af63c
Naman <agarwalnaman2406@gmail.com>
2022-01-20 21:14:29 +0530

    Sorting, Searching, Filtering & Pagination

--- src/Components/Favourite.js
+++ src/Components/Favourite.js
@@ -7,65 +7,165 @@ export default class Favourite extends Component {
         super()
         this.state = {
             genres: [],
-            currgen: 'All Genres'
+            currgen: 'All Genres',
+            movies:[],
+            currText:'',
+            limit:5,
+            currPage:1
         }
     }
 
-    render() {
-        const movie = movies.results;
+    componentDidMount(){
         let genreids = {28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
                         27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'};
-        
+        let data = JSON.parse(localStorage.getItem('movies') ||  "[]");
         let temp = [];
-        movie.forEach((movieObj) => {
+        data.forEach((movieObj) => {
             if(!temp.includes(genreids[movieObj.genre_ids[0]])){
                 temp.push(genreids[movieObj.genre_ids[0]]);
             }
         })
         temp.unshift("All Genres");
-        // this.setState({
-        //     genres:[...temp]
-        // })
+        this.setState({
+            genres:[...temp],
+            movies:[...data]
+        })
+    }
+
+    handleGenreChange=(genre)=>{
+        this.setState({
+            currgen:genre
+        })
+    }
+
+    sortPopularityDesc=()=>{
+        let temp = this.state.movies;
+        temp.sort(function(objA, objB){
+            return objB.popularity - objA.popularity
+        })
+        this.setState({
+            movies:[...temp]
+        })
+    }
+
+    sortRatingDesc=()=>{
+        let temp = this.state.movies;
+        temp.sort(function(objA, objB){
+            return objB.vote_average - objA.vote_average
+        })
+        this.setState({
+            movies:[...temp]
+        })
+    }
+
+    sortPopularityAsc=()=>{
+        let temp = this.state.movies;
+        temp.sort(function(objA, objB){
+            return objA.popularity - objB.popularity
+        })
+        this.setState({
+            movies:[...temp]
+        })
+    }
+
+    sortRatingAsc=()=>{
+        let temp = this.state.movies;
+        temp.sort(function(objA, objB){
+            return objA.vote_average - objB.vote_average
+        })
+        this.setState({
+            movies:[...temp]
+        })
+    }
+
+    handlePageChange=(page)=>{
+        this.setState({
+            currPage:page
+        })
+    }
+
+    handleDelete=(id)=>{
+        let newArr = [];
+        newArr = this.state.movies.filter((movieObj)=>movieObj.id!=id);
+        this.setState({
+            movies:[...newArr]
+        })
+        localStorage.setItem("movies",JSON.stringify(newArr));
+    }
+
+    render() {
+        let genreids = {28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
+                        27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'};
+        
+        let filterArr = [];
+        
+            // if(this.state.currgen=="All Genres"){
+            //     filterArr = this.state.movies
+            // }
+            // else{
+            //     filterArr = this.state.movies.filter((movieObj)=>genreids[movieObj.genre_ids[0]]==this.state.currgen);
+            // }
+
+        if(this.state.currText===''){
+            filterArr=this.state.movies
+        }else{
+            filterArr=this.state.movies.filter((movieObj)=>{
+                let title = movieObj.original_title.toLowerCase();
+                return title.includes(this.state.currText.toLowerCase());
+            })
+        }
+
+        if(this.state.currgen!="All Genres"){
+            filterArr = this.state.movies.filter((movieObj)=>genreids[movieObj.genre_ids[0]]==this.state.currgen);
+        }
+
+        let pages = Math.ceil(filterArr.length/this.state.limit);
+        let pagesArr = [];
+        for(let i=1;i<=pages;i++){
+            pagesArr.push(i);
+        }
+        let si = (this.state.currPage-1)*this.state.limit;
+        let ei = si + this.state.limit;
+        filterArr = filterArr.slice(si, ei);
         return (
             <div>
                 <>
                     <div className='main'>
                         <div className='row'>
-                            <div className='col-3'>
+                            <div className='col-lg-3 col-sm-12'>
                                 <ul className="list-group favourites-genres">
                                     {
-                                        temp.map((genre)=>(
+                                        this.state.genres.map((genre)=>(
                                             this.state.currgen == genre ?
                                             <li className="list-group-item" style={{background:'#3f51b5', color:'white', fontweight:'bold'}}>{genre}</li>
-                                            : <li className="list-group-item" style={{background:'white', color:'#3f51b5'}}>{genre}</li>
+                                            : <li className="list-group-item" style={{background:'white', color:'#3f51b5'}} onClick={()=>this.handleGenreChange(genre)}>{genre}</li>
                                         ))
                                     }
                                 </ul>
                             </div>
-                            <div className='col-9'>
+                            <div className='col-lg-9 favourites-table col-sm-12'>
                                 <div className='row favourites-table'>
-                                    <input type="text" className='input-group-text col' placeholder='Search'></input>
-                                    <input type="number" className='input-group-text col'placeholder='Rows Count'></input>
+                                    <input type="text" className='input-group-text col' placeholder='Search' value={this.state.currText} onChange={(e)=>this.setState({currText:e.target.value})}></input>
+                                    <input type="number" className='input-group-text col'placeholder='Rows Count' value={this.state.limit} onChange={(e)=>{this.setState({limit:e.target.value})}}></input>
                                 </div>
                                 <table class="table">
                                     <thead>
                                         <tr>
                                         <th scope="col">Title</th>
                                         <th scope="col">Genre</th>
-                                        <th scope="col">Popularity</th>
-                                        <th scope="col">Rating</th>
+                                        <th scope="col"><i class="fas fa-sort-up" onClick={this.sortPopularityDesc}></i>Popularity<i class="fas fa-sort-down" onClick={this.sortPopularityAsc}></i></th>
+                                        <th scope="col"><i class="fas fa-sort-up" onClick={this.sortRatingDesc}></i>Rating<i class="fas fa-sort-down" onClick={this.sortRatingAsc}></i></th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         {
-                                           movie.map((movieObj) => (
+                                           filterArr.map((movieObj) => (
                                                 <tr>
-                                                    <td><img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} alt={movieObj.title} style={{width:'5rem'}} /></td>
-                                                    <td>{movieObj.original_title}</td>
+                                                    <td><img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} alt={movieObj.title} style={{width:'5rem'}} />{movieObj.original_title}</td>
                                                     <td>{genreids[movieObj.genre_ids[0]]}</td>
                                                     <td>{movieObj.popularity}</td>
                                                     <td>{movieObj.vote_average}</td>
-                                                    <td><button type="button" class="btn btn-danger">Delete</button></td>
+                                                    <td><button type="button" class="btn btn-danger" onClick={(()=>this.handleDelete(movieObj.id))}>Delete</button></td>
                                                 </tr>
                                            )) 
                                         }
@@ -73,11 +173,11 @@ export default class Favourite extends Component {
                                 </table>
                                 <nav aria-label="Page navigation example">
                                     <ul class="pagination">
-                                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                                        {
+                                            pagesArr.map((page)=>(
+                                                <li class="page-item"><a class="page-link" href="#" onClick={()=>this.handlePageChange(page)}>{page}</a></li>
+                                            ))
+                                        }
                                     </ul>
                                 </nav>
                             </div>
